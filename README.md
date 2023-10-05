31e8665bff4abc27a74946e3cae9b18e5bd6aea6
ValerioCarbone <valeriocarbone10@gmail.com>
2023-10-05 21:22:20 +0200

    CHANGE-pseudo code order

--- README.md
+++ README.md
@@ -1,13 +1,31 @@
-- Chiedere all'utente il numero di km che vuole percorrere
-    - Creare un input html dove chiedere all'utente i km che vuole percorrere
-    - Inizializzare una variabile con i km che vuole percorrere
+ - Creare l'evento click
+     - Far selezionare all'utente la fascia di età in cui si trova
+         - Creare un select nell'html dove l'utente selezionerà la sua fascia di età
+         - Inizializzare una variabile a seconda della scelta dell'utente 
 
-- Far selezionare all'utente la fascia di età in cui si trova
-    - Creare un select nell'html dove l'utente selezionerà la sua fascia di età
-    - Inizializzare una variabile a seconda della scelta dell'utente    
+     - Chiedere all'utente il numero di km che vuole percorrere
+         - Creare un input html dove chiedere all'utente i km che vuole percorrere
+         - Inizializzare una variabile richiamando l'elemento per id
+         - Richiamare il value di quest'elemento    
+    
 
-- In base ai km e alla sua età calcolare il prezzo finale del biglietto applicando lo sconto se dovuto
-    - Inizializzare una variabile che sarà la moltiplicazione dei km che l'utente vuole percorrere per il costo per km e applicando ,se dovuto, lo sconto 
+     - Chiedere all'utente nome e cognome
+         - Inizializzare  una variabile con il nome e cognome dell'utente
 
-- Creare un form dove l'uente inserirà il suo nome, i km che vuole percorrere, l'età
-- Creare un recap dei dati inseriti
+     - In base ai km e alla sua età calcolare il prezzo finale del biglietto applicando lo sconto, se dovuto
+         - Inizializzare una variabile che sarà la moltiplicazione dei km che l'utente vuole  percorrere per il costo per km
+         - Applicare, se dovuto, lo sconto
+         - Formattare il prezzo finale in una cifra con max 2 decimali  
+         - Stampare in console il risultato quando l'utente cliccherà il bottone
+    
+
+     - Impedire all'utente di ottenere un risultato se non inserisce dati corretti
+
+     - Creare un form dove l'utente inserirà il suo nome, i km che vuole percorrere, l'età
+     - Creare un recap dei dati inseriti
+         - Inizializzare una variabile richiamando l'elemento html corrispettivo
+         - Modificare l'elemento html con il nome utente
+         - Modificare l'elemento html con i km selezionati
+         - Modificare l'elemento html con lo sconto applicato
+         - Modificare l'elemento html con il prezzo finale
+     - Stampare i dati
--- js/app.js
+++ js/app.js
@@ -1,59 +1,41 @@
-// - Chiedere all'utente il numero di km che vuole percorrere
-//     - Creare un input html dove chiedere all'utente i km che vuole percorrere
-//     - Inizializzare una variabile richiamando l'elemento per id
-//     - Richiamare il value di quest'elemento 
-
-// - Far selezionare all'utente la fascia di età in cui si trova
-//     - Creare un select nell'html dove l'utente selezionerà la sua fascia di età
-//     - Inizializzare una variabile a seconda della scelta dell'utente    
-
-// - In base ai km e alla sua età calcolare il prezzo finale del biglietto applicando lo sconto, se dovuto
-//      - Inizializzare una variabile che sarà la moltiplicazione dei km che l'utente vuole  percorrere per il costo per km
-
-//  - Inizializzare  una variabile con il nome e cognome dell'utente
-
-//      - Applicare, se dovuto, lo sconto
-//      - Formattare il prezzo finale in una cifra con max 2 decimali              
-//      - Stampare in console il risultato quando l'utente cliccherà il bottone
-
-// - Creare un form dove l'uente inserirà il suo nome, i km che vuole percorrere, l'età
-// - Creare un recap dei dati inseriti
-//      - Inizializzare una variabile richiamando l'elemento html corrispettivo
-//      - Modificare l'elemento html con il nome utente
-
-
-//      - Inizializzare una variabile richiamando l'elemento html corrispettivo
-//      - Modificare l'elemento html con i km selezionati
-
-//      - Inizializzare una variabile richiamando l'elemento html corrispettivo
-//      - Modificare l'elemento html con lo sconto applicato
-
-
-//      - Inizializzare una variabile richiamando l'elemento html corrispettivo
-//      - Modificare l'elemento html con il prezzo finale
-
+// - Creare l'evento click
 
 const btn = document.querySelector('.btn-clc');
 btn.addEventListener('click', function () {
+
+    // - Far selezionare all'utente la fascia di età in cui si trova
+    //     - Creare un select nell'html dove l'utente selezionerà la sua fascia di età
+    //     - Inizializzare una variabile a seconda della scelta dell'utente 
     let userAge
     userAge = document.getElementById('age')
-    userAge = parseInt(userAge.value)
-    
+    userAge = userAge.value
+
+    // - Chiedere all'utente il numero di km che vuole percorrere
+    //     - Creare un input html dove chiedere all'utente i km che vuole percorrere
+    //     - Inizializzare una variabile richiamando l'elemento per id
+    //     - Richiamare il value di quest'elemento    
     let userKm
     userKm = document.getElementById('km')
     userKm = parseFloat(userKm.value)
-    
+
+    // - Chiedere all'utente nome e cognome
+    //      - Inizializzare  una variabile con il nome e cognome dell'utente
     let userName = document.getElementById('name-surname');
     userName = userName.value;
-    
-    let discountU18
-    
-    let discountO65
-    
-    let recapDiscount
-    
+
+
+    // - In base ai km e alla sua età calcolare il prezzo finale del biglietto applicando lo sconto, se dovuto
+    //      - Inizializzare una variabile che sarà la moltiplicazione dei km che l'utente vuole  percorrere per il costo per km
     let ticketPrice = userKm * 0.21
-    
+
+
+    let discountU18
+
+    let discountO65
+
+    let recapDiscount
+
+    //      - Applicare, se dovuto, lo sconto
     if (userAge === 0) {
         discountU18 = ticketPrice * 0.2
         console.log('Lo sconto U18 applicato sarà: ' + discountU18)
@@ -73,21 +55,60 @@ btn.addEventListener('click', function () {
     console.log('Questi è lo sconto: ' + recapDiscount)
 
     console.log('Questi sono i km che hai selezionato: ' + userKm)
-    
+
+    //      - Formattare il prezzo finale in una cifra con max 2 decimali  
     ticketPrice = ticketPrice.toFixed(2)
+
+    //      - Stampare in console il risultato quando l'utente cliccherà il bottone
     console.log(ticketPrice)
 
+    //      - Impedire all'utente di ottenere un risultato se non inserisce dati corretti
+    if (userKm < 0) {
+        userKm = 'Inserire i dati'
+        userName = 'Inserire i dati'
+        recapDiscount = 'Inserire i dati'
+        ticketPrice = 'Inserire i dati'
+    }
+    else if (userName = '') {
+        userKm = 'Inserire i dati'
+        userName = 'Inserire i dati'
+        recapDiscount = 'Inserire i dati'
+        ticketPrice = 'Inserire i dati'
+    }
+    else if (userKm = 'NaN') {
+        userKm = 'Inserire i dati'
+        userName = 'Inserire i dati'
+        recapDiscount = 'Inserire i dati'
+        ticketPrice = 'Inserire i dati'
+    }
+
+    // - Creare un form dove l'uente inserirà il suo nome, i km che vuole percorrere, l'età
+    // - Creare un recap dei dati inseriti
+    //      - Inizializzare una variabile richiamando l'elemento html corrispettivo
+    //      - Modificare l'elemento html con il nome utente
     let recapNameDOMElement = document.getElementById('user_name')
     recapNameDOMElement.innerHTML = userName
 
+    //      - Inizializzare una variabile richiamando l'elemento html corrispettivo
+    //      - Modificare l'elemento html con i km selezionati
     let recapKmDOMElement = document.getElementById('user-km')
     recapKmDOMElement.innerHTML = userKm
 
+    //      - Inizializzare una variabile richiamando l'elemento html corrispettivo
+    //      - Modificare l'elemento html con lo sconto applicato
     let recapDiscountDOMElement = document.getElementById('user-discount')
     recapDiscountDOMElement.innerHTML = recapDiscount
 
+    //      - Inizializzare una variabile richiamando l'elemento html corrispettivo
+    //      - Modificare l'elemento html con il prezzo finale
     let recapFinalPriceDOMElement = document.getElementById('user-final-price')
     recapFinalPriceDOMElement.innerHTML = ticketPrice
+
+
+
+
+
+
 
 
 }
