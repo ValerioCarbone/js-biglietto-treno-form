ab7245a18dac0c87614a3fee5a21bd0c4a3fa2b7
ValerioCarbone <valeriocarbone10@gmail.com>
2023-10-05 15:56:00 +0200

    ADD-Html + js userAge,userKm + discount

---
+++ js/app.js
@@ -0,0 +1,39 @@
+// - Chiedere all'utente il numero di km che vuole percorrere
+//     - Creare un input html dove chiedere all'utente i km che vuole percorrere
+//     - Inizializzare una variabile richiamando l'elemento per id
+//     - Richiamare il value di quest'elemento 
+        let userKm = document.getElementById('km')
+        userKm = parseFloat(userKm.value)         
+        console.log(userKm)
+        
+// - Far selezionare all'utente la fascia di età in cui si trova
+//     - Creare un select nell'html dove l'utente selezionerà la sua fascia di età
+//     - Inizializzare una variabile a seconda della scelta dell'utente    
+        let userAge = document.getElementById('age')
+        userAge = parseInt(userAge.value)
+
+// - In base ai km e alla sua età calcolare il prezzo finale del biglietto applicando lo sconto, se dovuto
+//      - Inizializzare una variabile che sarà la moltiplicazione dei km che l'utente vuole      percorrere per il costo per km
+        let ticketPrice = userKm * 0.21
+
+//      - Applicare, se dovuto, lo sconto
+        if (userAge === 0 ){
+            const discountU18 = ticketPrice * 0.2
+            console.log('Lo sconto U18 applicato sarà: '+ discountU18)
+            ticketPrice = ticketPrice - discountU18
+            
+        }
+        else if (userAge === 2) {
+            const discountO65 = ticketPrice * 0.4
+            console.log('Lo sconto O65 applicato sarà: '+ discountO65)
+            ticketPrice = ticketPrice - discountO65 
+        }
+        console.log(ticketPrice)
+
+//      - Stampare in console il risultato quando l'utente cliccherà il bottone
+        const btn = document.querySelector('.btn-clc')
+
+// - Creare un form dove l'uente inserirà il suo nome, i km che vuole percorrere, l'età
+
+
+// - Creare un recap dei dati inseriti
