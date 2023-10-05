ae5df4a9a23aa9f96aeabaec675764d2892c20df
ValerioCarbone <valeriocarbone10@gmail.com>
2023-10-05 16:25:23 +0200

    CHANGE-Inserito tutto dopo il click

--- js/app.js
+++ js/app.js
@@ -2,36 +2,45 @@
 //     - Creare un input html dove chiedere all'utente i km che vuole percorrere
 //     - Inizializzare una variabile richiamando l'elemento per id
 //     - Richiamare il value di quest'elemento 
-        let userKm = document.getElementById('km')
-        userKm = parseFloat(userKm.value)         
-        console.log(userKm)
+        let userKm = document.getElementById('km');
+        userKm = parseFloat(userKm.value);         
+        
         
 // - Far selezionare all'utente la fascia di età in cui si trova
 //     - Creare un select nell'html dove l'utente selezionerà la sua fascia di età
 //     - Inizializzare una variabile a seconda della scelta dell'utente    
-        let userAge = document.getElementById('age')
-        userAge = parseInt(userAge.value)
+        let userAge = document.getElementById('age');
+        userAge = parseInt(userAge.value);
+        
 
 // - In base ai km e alla sua età calcolare il prezzo finale del biglietto applicando lo sconto, se dovuto
 //      - Inizializzare una variabile che sarà la moltiplicazione dei km che l'utente vuole      percorrere per il costo per km
-        let ticketPrice = userKm * 0.21
+        let ticketPrice = userKm * 0.21;
 
 //      - Applicare, se dovuto, lo sconto
-        if (userAge === 0 ){
-            const discountU18 = ticketPrice * 0.2
-            console.log('Lo sconto U18 applicato sarà: '+ discountU18)
-            ticketPrice = ticketPrice - discountU18
-            
-        }
-        else if (userAge === 2) {
-            const discountO65 = ticketPrice * 0.4
-            console.log('Lo sconto O65 applicato sarà: '+ discountO65)
-            ticketPrice = ticketPrice - discountO65 
-        }
-        console.log(ticketPrice)
-
+//      - Formattare il perzzo finale in una cifra con max 2 decimali              
 //      - Stampare in console il risultato quando l'utente cliccherà il bottone
         const btn = document.querySelector('.btn-clc')
+        btn.addEventListener('click', function() {
+            if (userAge === 0 ){
+                const discountU18 = ticketPrice * 0.2
+                console.log('Lo sconto U18 applicato sarà: '+ discountU18)
+                ticketPrice = ticketPrice - discountU18
+                
+            }
+            else if (userAge === 2) {
+                const discountO65 = ticketPrice * 0.4
+                console.log('Lo sconto O65 applicato sarà: '+ discountO65)
+                ticketPrice = ticketPrice - discountO65 
+            }
+            
+            console.log('Questi sono i km che hai selezionato: ' + userKm)
+            ticketPrice = ticketPrice.toFixed(2)
+            console.log(ticketPrice)  
+        }
+        )
+
+        
 
 // - Creare un form dove l'uente inserirà il suo nome, i km che vuole percorrere, l'età
 
